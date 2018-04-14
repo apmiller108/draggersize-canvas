a18a70706eb565dbfe97c01464799f8ad088da46
Alex Miller <apmiller108@yahoo.com>
2018-04-14 01:21:59 -0400

    Setup webpack and package.json script

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname)
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
